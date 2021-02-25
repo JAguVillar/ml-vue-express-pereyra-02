4bdd85a9c0b3ef798960211cfb39a135062b78e3
JAguVillar <jaguvillar@gmail.com>
2021-02-25 00:05:24 -0300

    lapll

---
+++ client/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+  </div>
+</template>
+
+<script>
+// const API_URL = "https://api.mercadolibre.com/sites/MLA/search?q=ssd"
+
+export default {
+  data () {
+    return {
+      variable: 'hola',
+      items: ['sdas']
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+    console.log(this.variable)
+    this.fetch()
+  },
+  methods: {
+    fetch () {
+      this.$axios.get("https://api.mercadolibre.com/sites/MLA/search?q=ssd")
+        .then((response) => {
+          // handle success
+          this.items = response.data.results
+          console.log(this.items)
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error)
+        })
+        .then(() => {
+          // always executed
+        })
+    }
+  }
+};
+</script>
+
