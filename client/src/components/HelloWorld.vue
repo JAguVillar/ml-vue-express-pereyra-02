514ba59ccb8a6af1a664eae693734f8b7f1a695b
JAguVillar <jaguvillar@gmail.com>
2021-02-26 02:48:25 -0300

    avances

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,24 +1,76 @@
 <template>
   <v-container>
+    <v-text-field
+      label="Buscar Articulo de MercadoLibre"
+      placeholder="Ej. Descarrilador Shimano"
+      filled
+      v-model="busquedaInput"
+      @keydown.enter="buscarML"
+    >
+      <v-icon slot="append">
+        mdi-shopping-search
+      </v-icon>
+    </v-text-field>
+    <span>Mostrar articulo con:</span>
+    <v-chip>Mas ventas </v-chip>
+    <v-chip>asdasd</v-chip>
+    <v-chip>asdasd</v-chip>
     <v-data-table
-      loading
+      :loading="loadingMainTabla"
+      loading-text="Cargando... Espere"
       multi-sort
-      dense
       :headers="headers"
       :items="items"
-      :items-per-page="5"
+      :items-per-page="10"
       class="elevation-1"
     >
-      <template v-slot:item.permalink="{ item }">
-        <a :href="item.permalink">
-          {{ item.permalink }}
-        </a>
+      <template v-slot:item.price="{ item }">
+        $ {{ item.price}}
       </template>
-      <template v-slot:item.thumbnail="{ item }">
-        <a :href="item.thumbnail">
-          {{ item.thumbnail }}
-        </a>
+      <template v-slot:item.links="{ item }">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              @click="abrirPestania(item.permalink)"
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-link-variant
+            </v-icon>
+          </template>
+          <span>Ir al articulo</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              @click="abrirPestania(item.thumbnail)"
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-image
+            </v-icon>
+          </template>
+          <span>Ver imagen</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              @click="abrirPestania(item.seller.permalink)"
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-account-circle
+            </v-icon>
+          </template>
+          <span>Pagina del vendedor</span>
+        </v-tooltip>
+
       </template>
+      <!-- <template v-slot:item.seller.permalink="{ item }">
+        <a :href="item.seller.permalink">
+          {{ item.seller.pernalink }}
+        </a>
+      </template> -->
     </v-data-table>
   </v-container>
 </template>
@@ -27,45 +79,48 @@
 export default {
   data () {
     return {
-      variable: 'hola',
+      busquedaInput: "",
       headers: [
         {
-          text: 'ID MLA',
-          align: 'start',
+          text: "ID MLA",
+          align: "start",
           sortable: false,
-          value: 'id',
+          value: "id",
         },
-        { text: 'Articulo', value: 'title' },
-        { text: 'Precio', value: 'price' },
-        { text: 'Link', value: 'permalink' },
-        { text: 'Imagen', value: 'thumbnail' },
-        { text: 'Vendedor', value: 'seller.permalink' },
+        { text: "Articulo", value: "title" },
+        { text: "Precio", value: "price" },
+        { text: "Links", value: "links", sortable: false },
       ],
       items: [],
-      loading: true
-    }
+      loadingMainTabla: false,
+    };
   },
-  created () {
-  },
-  mounted () {
-    this.fetch()
-  },
+  created () { },
+  mounted () { },
   methods: {
-    fetch () {
-      this.$axios.get("https://api.mercadolibre.com/sites/MLA/search?q=ssd")
+    buscarML () {
+      this.loadingMainTabla = true;
+      this.$axios
+        .get(
+          "https://api.mercadolibre.com/sites/MLA/search?q=" +
+          encodeURIComponent(this.busquedaInput)
+        )
         .then((response) => {
           // handle success
-          this.items = response.data.results
-          console.log(this.items)
-          this.loading = false
+          this.items = response.data.results;
+          console.log(this.items);
+          this.loadingMainTabla = false;
         })
         .catch((error) => {
           // handle error
-          console.log(error)
+          console.log(error);
         })
         .then(() => {
           // always executed
-        })
+        });
+    },
+    abrirPestania (link) {
+      window.open(link, "_blank");
     },
     // calcular () {
     //   this.items.forEach(element => {
@@ -77,5 +132,3 @@ export default {
   },
 };
 </script>
-
-
